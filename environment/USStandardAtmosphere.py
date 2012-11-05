896fa7485f417383f9ae5b6927a024e892eb1c8a
Matt Cowan <matthew.lj.cowan@gmail.com>
2012-11-04 18:10:15 -0800

    Sorted out inheritance issues. WorkingEnvironment and USStandardAtmosphere are fully operational and debugged.

--- environment/USStandardAtmosphere.py
+++ environment/USStandardAtmosphere.py
@@ -1,124 +1,111 @@
 """
 USStandardAtmosphere calculates atmospheric properties based on the 1975 model.
-
-Note that this model differs slightly from the US standard atmospheric model. Go to wikipedia for more details.
+Go to wikipedia for more details.
 
 ALL UNITS ARE SI BASE UNITS
-
-Constructors:-
-
-Implementation:-
 
 """
 
 import math
+from WorkingAtmosphere import WorkingAtmosphere
 
-class USStandardAtmosphere:
+class USStandardAtmosphere(WorkingAtmosphere):
 
 	#Constructors
-	def __init__(self, alt=0, tOffset=0):
-		self.altitude = alt
-		self.temperature_offset = tOffset
-		
-		self.temperature = None
-		self.pressure = None
-		self.density = None
-		self.speed_of_sound = None
-		self.kinematic_viscosity = None
+	def __init__(self, alt=0):
+		super().__init__(alt)		
+		#self.temperature_offset = tOffset
+		self.temperature_offset = 0
 		
 		self.makeEnvironment()
-		self.base_layer = 'troposphere'
+
+
+	#Methods
+	def makeEnvironment(self):
+
+		base_layer = 0
 		self.gravity = 9.81
 
 		#Private data for to define model
 		__model_max_altitude = 87000
-		__atmosphere_layers = {0:1, 11000:2, 20000:3, 32000:4, 47000:5, 51000:6, 71000:7}
+		__atmosphere_layers = {0:0, 11000:1, 20000:2, 32000:3, 47000:4, 51000:5, 71000:6}
 		__layer_base_data = {
 			0:{'temp':288.15, 'lapse':-0.0065, 'press':101325},
-			1:{'temp':216.65, 'lapse':0.0, 'press':22632.1},
+			1:{'temp':216.65, 'lapse':0, 'press':22632.1},
 			2:{'temp':216.65, 'lapse':0.001, 'press':5474.89},
 			3:{'temp':228.65, 'lapse':0.0028, 'press':868.019},
-			4:{'temp':270.65, 'lapse':0.0, 'press':110.906},
+			4:{'temp':270.65, 'lapse':0, 'press':110.906},
 			5:{'temp':270.65, 'lapse':-0.0028, 'press':66.9389},
 			6:{'temp':214.65, 'lapse':-0.002, 'press':3.95642},
 			}
-		__gas_constant = 8.31432e3 
+		__gas_constant = 8.31432#e3 
 		__air_molar_mass = 0.0289644
 		__specific_heat_ratio = 1.4
-		__visc_lambda = 1.51204129
+		__visc_lambda = 1.51204129e-6
 		__visc_sutherland_const = 120.0
 
-	#Methods
-	def makeEnvironment(self):
-		if altitude > __model_max_altitude:
+		if self.altitude > __model_max_altitude:
 			raise ModelExtrapolationError
-			
-		for layer in __atmosphereLayer.keys():
-			if altitude > layer:
-				baselayer = __atmpsphere_layers[layer]
-				base_alt = layer
 		
+		layerKeys = __atmosphere_layers.keys()
+		layerKeys = list(layerKeys)
+		layerKeys.sort()	
+		for layer in layerKeys:
+			if self.altitude >= layer:
+				base_layer = __atmosphere_layers[layer]
+				base_alt = layer
 		base_temp = __layer_base_data[base_layer]['temp']
 		base_lapse = __layer_base_data[base_layer]['lapse']
 		base_press = __layer_base_data[base_layer]['press']
 		
-		self.temperature = base_temp + self.altitude * base_lapse + self.temperature_offset
+		self.temperature = base_temp + base_lapse * (self.altitude - base_alt) + self.temperature_offset
 		
-		if base-lapse == 0: 		
+		if base_lapse == 0: 		
 			self.pressure = base_press * math.exp((-1 * self.gravity * __air_molar_mass * (self.altitude - base_alt)) / (__gas_constant * base_temp))
-		elif
+		else:
 			self.pressure = base_press * (base_temp / self.temperature) ** (self.gravity * __air_molar_mass / __gas_constant / base_lapse)
-
+		
 		self.density = __air_molar_mass * self.pressure / __gas_constant / self.temperature
-		
-		self.speed_of_sound = (__specific_heat_ratio * __gas_constant * self.temperature) ** 0.5
-		
-		self.kinematic_viscosity = __visc_lambda * self.temperature**(3/2) / (self.temperature + __visc_sutherland_const)
 
-	def getEnvironment(self):
-		enviro = {'alt':self.altitude, 'temp':self.temperature, 'dens':self.density, 'press':self.pressure, 
-			'sos':self.speed_of_sound, 'kvisc':self.kinematic_viscosity}
-		return enviro
+		self.speed_of_sound = (__specific_heat_ratio * __gas_constant * self.temperature / __air_molar_mass) ** 0.5
 
-	def getDynamicPressure(self, velocity):
-		dynamic_press = 0.5 * self.density * velocity**2
-		return dynamic_press
+		self.dynamic_viscosity = __visc_lambda * self.temperature**(3/2) / (self.temperature + __visc_sutherland_const)
 
-	def getReynoldsNumber(self, velocity, refLength):
-		re_num = self.density * velocity * refLength / self.kinematic_viscosity
-		return re_num
-
-	def getMachNumber(self, velocity):
-		ma_num = velocity / self.speed_of_sound
-		return ma_num
-		
-	#Overloads
-	def __str__(self):
-		outString = 
-		return outString
 
 if __name__ == '__main__':
+	print('\n\n')
 	bookTempSL = 288.16
 	bookDensSL = 1.2250
 	aa = USStandardAtmosphere()
 	tempErrorSL = (aa.temperature - bookTempSL) / bookTempSL
 	densErrorSL = (aa.density - bookDensSL) / bookDensSL
+	output = 'T = %s ... rho = %s' % (aa.temperature, aa.density)
+	output += '\nSL: temp error = %.5f ... dens error = %.5f' % (tempErrorSL, densErrorSL)
+	print(output)
 	
 	bookTemp5K = 255.69
 	bookDens5K = 7.6343e-1
 	bb = USStandardAtmosphere(5000)
 	tempError5K = (bb.temperature - bookTemp5K) / bookTemp5K
 	densError5K = (bb.density - bookDens5K) / bookDens5K
+	output = 'T = %s ... rho = %s' % (bb.temperature, bb.density)
+	output += '\n5K: temp error = %.5f ... dens error = %.5f' % (tempError5K, densError5K)
+	print(output)
 	
 	bookTemp20K = 216.66
 	bookDens20K = 8.8909e-2
 	cc = USStandardAtmosphere(20000)
 	tempError20K = (cc.temperature - bookTemp20K) / bookTemp20K
 	densError20K = (cc.density - bookDens20K) / bookDens20K
-
+	output = 'T = %s ... rho = %s' % (cc.temperature, cc.density)	
+	output += '\n20K: temp error = %.5f ... dens error = %.5f' % (tempError20K, densError20K)
+	print(output)
 	
-	bookTemp50K = 282.66
-	bookDens50K = 1.0829e-3
+	bookTemp50K = 270.65
+	bookDens50K = 0.000978
 	dd = USStandardAtmosphere(50000)
 	tempError50K = (dd.temperature - bookTemp50K) / bookTemp50K
 	densError50K = (dd.density - bookDens50K) / bookDens50K
+	output = 'T = %s ... rho = %s' % (dd.temperature, dd.density)	
+	output += '\n50K: temp error = %s ... dens error = %s' % (tempError50K, densError50K)
+	print(output)
--- environment/WorkingAtmosphere.py
+++ environment/WorkingAtmosphere.py
@@ -1,8 +1,13 @@
 """
 WorkingEnvironment defines a superclass for all working fluid environments.
+
+This class initializes the minimum required atmospheric variables for the mission and physics simulators.
+It also defines some helpful tools that make use of these essential variables.
+At this level, the atmospheric model to be used does not favour one set of units over any other. 
 """
-class WorkingEnvironment:
-	
+class WorkingAtmosphere:
+
+	### CONSTRUCTOR	
 	"""
 	The constructor sets the desired altitude and initializes all data to None.	
 	"""
@@ -12,27 +17,30 @@ class WorkingEnvironment:
 		self.density = None
 		self.pressure = None
 		self.speed_of_sound = None
-		self.kinematic_viscosity = None
+		self.dynamic_viscosity = None
 	
+	### METHODS
+	"""
+	This method should populate the variables defined in the constructor, however it must be overloaded in a subclass that defines which model is to be used.
+	"""
 	def makeEnvironment(self):
 		raise NotImplementedError("This method must be overloaded in a subclass to define the model to be used")
-	
-	def getEnvironment(self):
-		enviro = {'alt':altitude, 'temp':temperature, 'dens':density, 'press':pressure, 
-			'sos':speed_of_sound, 'kvisc':kinematic_viscosity}
-		return enviro
-		
+
 	def getDynamicPressure(self, velocity):
-		dynamic_press = 0.5 * density * velocity**2
+		dynamic_press = 0.5 * self.density * velocity**2
 		return dynamic_press
-		
-	def getReynoldsNumber(self, velocity, surfaceLength):
-		re_num = density * velocity * surfaceLength / kinematic_viscosity
+
+	def getReynoldsNumber(self, velocity, refLength):
+		re_num = self.density * velocity * refLength / self.dynamic_viscosity
 		return re_num
-	
+
 	def getMachNumber(self, velocity):
-		ma_num = velocity / speed_of_sound
+		ma_num = velocity / self.speed_of_sound
 		return ma_num
+
+	### SYSTEM OVERLOADS
+	
+
 
 #Testing script for this module run as standalone
 if __name__ == '__main__':
